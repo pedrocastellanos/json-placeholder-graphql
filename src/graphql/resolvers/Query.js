484969e2ad3d67bec567e7270fc9bd0691075ed6
pedrocastellanos <53188140+pedrocastellanos@users.noreply.github.com>
2021-07-27 12:54:46 -0400

    Update Query.js

--- src/graphql/resolvers/Query.js
+++ src/graphql/resolvers/Query.js
@@ -1,8 +1,6 @@
 const {getPosts, getUsers, getPhotos, getAlbums, getComments, getTodos} = require('../controllers/getAll.controller')
 const {getPost ,getPostComments ,getUser ,getUserPosts ,getUserTodos ,getUserAlbums ,getPhoto ,getAlbum ,getComment ,getTodo} = require('../controllers/getOne.controller')
 
-console.log(getPost)
-
 const Query = {
 	/*----------Get All----------*/
     posts: ()=>getPosts(),
@@ -13,15 +11,15 @@ const Query = {
     todos: ()=>getTodos(),
     
     /*----------Get One----------*/
-    post: (_,postId)=> getPost(postId),
-    post_comments: (_, postId)=> getPostComments(postId),
-    user: (_, userId)=> getUser(userId),
-    user_posts: (_, userId)=> getUserPosts(userId),
-    user_todos: (_, userId)=> getUserTodos(userId),
-    user_albums: (_, userId)=> getUserAlbums(userId),
-    photo: (_, id)=> getPhoto(id),
-    album: (_, albumId)=> getAlbum(albumId),
-    todo: (_, id)=> getTodo(id)
+    post: (_,{postId})=> getPost(postId),
+    post_comments: (_, {postId})=> getPostComments(postId),
+    user: (_, {userId})=> getUser(userId),
+    user_posts: (_, {userId})=> getUserPosts(userId),
+    user_todos: (_, {userId})=> getUserTodos(userId),
+    user_albums: (_, {userId})=> getUserAlbums(userId),
+    photo: (_, {id})=> getPhoto(id),
+    album: (_, {albumId})=> getAlbum(albumId),
+    todo: (_, {id})=> getTodo(id)
 }
 
 module.exports = Query
